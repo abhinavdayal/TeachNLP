7a7d6fa86449df2c5cec0c283f00ec380137e74e
Lakshman511 <lakshman6302794256@gmail.com>
2021-10-01 18:08:35 +0530

    Language models are created

--- teachnlp/languagemodels/ngram.py
+++ teachnlp/languagemodels/ngram.py
@@ -3,28 +3,27 @@ from typing import List, Optional, Text
 
 class N_gram:
 
-    """
-    A class used used to represent n-gram language model for sentence generation.
-
+    """A class used used to represent n-gram language model for sentence generation.
+    The aim of am n-gram language model is to predict a word given previous n-1 words.
+    Note : tuple of n-1 words is referred as n-gram here. 
+    For eg: ('he','is') is a 3-gram because it predicts 3rd word given previous two words.
     ...
+
     Attributes
     ----------
     n : int
         A number reprsenting the model. 2 for bi-gram model and n for n-gram model.
 
     randomness_number : int
-        It represents the randomness of our model. If it is too high it may generate meaningless sentence.
-        If it is very less then it may generate same sentences again and again. Read n_gram_dict to better understand the use of this.
+        It represents the randomness of our model. If it is too high it may generate meaningless sentences.
+        If it is very less then it may generate the same sentences again and again. Read possible_next_words_dict to better understand the use of this.
 
-    n_gram_dict : {n-gram:[w1,w2,..,w(randomness_number)]}
-        It is a dictionary. It contains n-grams and list of words which are most probable to appear as next word.
-        n-gram is a tuple of n-1 words.
+    possible_next_words_dict : {n-gram:[w1,w2,..,w(randomness_number)]}
+        It is a dictionary. It contains every tuple of n-1 words as keys and list of most probable next words as values.
         For eg: (He,is):[a,good,nice] -> This can be a key-value pair for a 3-gram model with randomness_number = 3.
 
     vocabulary : set()
         Set of all words appear in training corpus.
-
-
 
 
     Methods
@@ -60,7 +59,7 @@ class N_gram:
         """
         self.n = n
         self.randomness_number = randomness_number
-        self.n_gram_dict = dict()
+        self.possible_next_words_dict = dict()
         self.vocabulary = set()
 
 
@@ -74,13 +73,17 @@ class N_gram:
         training_corpus : List[List[Text]]
             It is a list of sentences. Each sentence is a list of words.
         
-        This method scans the entire training corpus. It uses sliding window method for n-gram count. You can read about sliding window here https://www.geeksforgeeks.org/window-sliding-technique 
-        For an n-gram model. It takes window of size n-1. Window slides from left to right. In every iteration tuple(window) is the n-gram.
+        Description
+        -----------
+        It uses sliding window method for n-gram count. You can read about sliding window here https://www.geeksforgeeks.org/window-sliding-technique 
+        This method scans the entire training corpus. 
+        For an n-gram model. It takes window of size n-1. Window slides from left to right.
 
-        This method populates self.n_gram_dict with n-gram,list of top probable words.
         First we store (n-gram,{word:frequency}) in temp_n_gram_dict by scanning the entire training corpus.
-        Later self.n_gram_dict is populated with the help of temp_n_gram_dict.
+        Then self.possible_next_words_dict is populates with (n-gram:list of top probable words) key-value pairs using temp_n_gram_dict.
 
+        Sample Iterations
+        -----------------
         For eg: Let's see three iterations for a training sentence.
         Actual sentence : ['he','is','a','good','boy']
         We will add <EOS> add to this sentence
@@ -113,6 +116,7 @@ class N_gram:
 
             # loop to slide the window form left to right of the training sentence
             for ptr in range(len(sentence)):
+                self.vocabulary.add(window[ptr])
                 words_freqt_dict = temp_n_gram_dict.get(tuple(window), dict())
 
                 # frequency of current word should be incremented by 1 in temp_n_gram_dict[tiple(Window)]
@@ -121,11 +125,10 @@ class N_gram:
                 window.pop(0)
                 window.append(sentence[ptr])
 
-
-        # populating self.n_gram_dict with (n-gram:list of top randomness_number words) key-value pairs using temp_n_gram_dict 
+        # populating self.possible_next_words_dict with (n-gram:list of top randomness_number words) key-value pairs using temp_n_gram_dict
         for n_gram in temp_n_gram_dict:
             top_words = sorted(temp_n_gram_dict[n_gram], key=lambda x: temp_n_gram_dict[n_gram][x])
-            self.n_gram_dict[n_gram] = top_words[:self.randomness_number]
+            self.possible_next_words_dict[n_gram] = top_words[:self.randomness_number]
         print("Done!!!")
 
 
@@ -151,18 +154,30 @@ class N_gram:
 
     def strip_sentence(self, sentence: List[Text]) -> List[Text]:
 
-        """stripping the sentence Such that length of sentence becomes self.n - 1 
+        """Strips the sentence Such that length of sentence becomes self.n - 1 
         For eg: If we trained a 3-gram model The ['he','is','a','good'] will become ['a','good'] after stripping
         ...
 
-        Parameters
-        ----------
         Parameters
         ----------
         sentence: List[Text]
           list of words representing the sentence
         """
         return sentence[::-1][:self.n-1][::-1]
+
+
+    def process_sentence(self,sentence: List[Text]) -> List[Text]:
+        """This method applies stripping followed by padding
+        ...
+
+        Parameters
+        ----------
+        sentence: List[Text]
+          list of words representing the sentence
+        """
+        sentence = self.strip_sentence(sentence)
+        sentence = self.pad_sentence(sentence)
+        return sentence
 
 
 
@@ -179,11 +194,10 @@ class N_gram:
         beginning_words: List[Text]
           list of words
         """
-        beginning_words = self.strip_sentence(beginning_words)
-        beginning_words = self.pad_sentence(beginning_words)
-        if tuple(beginning_words) not in self.n_gram_dict:
+        beginning_words = self.process_sentence(beginning_words)
+        if tuple(beginning_words) not in self.possible_next_words_dict:
             return "<EOS>"
-        return random.choice(self.n_gram_dict[tuple(beginning_words)])
+        return random.choice(self.possible_next_words_dict[tuple(beginning_words)])
 
 
 
@@ -200,8 +214,7 @@ class N_gram:
         """
         sentence = beginning_words[:]
         window = beginning_words[:]
-        window = self.strip_sentence(window)
-        window = self.pad_sentence(window)
+        window = self.process_sentence(window)
         next_word = self.predict_next_word(window)
         while (next_word != "<EOS>"):
             sentence.append(next_word)
@@ -209,3 +222,5 @@ class N_gram:
             window.append(next_word)
             next_word = self.predict_next_word(window)
         return " ".join(sentence)
+
+
